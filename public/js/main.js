6eb2bd8853e1b1c94b74d00f64722e3750a13c3b
Allison Higginbotham <higgintop@gmail.com>
2015-06-10 17:29:41 -0500

    Fixed tabbing issue

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,33 @@
 $(function(){
-	var nav_height = $('nav').height();
+  var nav_height = $('nav').height();
 
-	// before any scrolling see where page is at
-	var initial_pos = $(window).scrollTop();
+  // before any scrolling see where page is at
+  var initial_pos = $(window).scrollTop();
 
-	if(initial_pos > nav_height) {
-		console.log("SHOULD SHOW SMALL MENU!!");
-		// display small menu
-		$('.small-menu').show();
-		$('.subscribe').hide();
-	}
-	else {
-		// display subscribe
-		$('.small-menu').hide();
-		$('.subscribe').show();
-	}
+  if(initial_pos > nav_height) {
+    console.log("SHOULD SHOW SMALL MENU!!");
+    // display small menu
+    $('.small-menu').show();
+    $('.subscribe').hide();
+  }
+  else {
+    // display subscribe
+    $('.small-menu').hide();
+    $('.subscribe').show();
+  }
 
-	$(window).scroll(function () {
-		var cur_pos = $(window).scrollTop();
+  $(window).scroll(function () {
+    var cur_pos = $(window).scrollTop();
 
-		if (cur_pos > nav_height){
-		  $('.subscribe').fadeOut(500, function() {
-	       $('.small-menu').fadeIn(500);
-	    });
-		}
-		else {
-		  $('.small-menu').fadeOut(500, function() {
-	       $('.subscribe').fadeIn(500);
-	    });		
-		}
-	});
+    if (cur_pos > nav_height){
+      $('.subscribe').fadeOut(500, function() {
+         $('.small-menu').fadeIn(500);
+      });
+    }
+    else {
+      $('.small-menu').fadeOut(500, function() {
+         $('.subscribe').fadeIn(500);
+      });    
+    }
+  });
 });
